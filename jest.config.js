4275a7b61a1d428a85cd3e4c499c60c696f7a64e
Tiago Gomes <tidgomes@gmail.com>
2024-01-30 11:00:45 -0300

    chore: config jest with ts-jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+const config = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: false,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
+
+module.exports = config
