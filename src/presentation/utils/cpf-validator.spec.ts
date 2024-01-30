96d4989518106a0e1f8eb7fc6772dd8ed1c1c23a
Tiago Gomes <tidgomes@gmail.com>
2024-01-30 15:42:46 -0300

    test: ensure CpfValidator returns false if numbers only cpf does not match pattern

--- src/presentation/utils/cpf-validator.spec.ts
+++ src/presentation/utils/cpf-validator.spec.ts
@@ -1,11 +1,19 @@
 import { CpfValidator } from './cpf-validator'
 
 describe('CpfValidator', () => {
-  test('should return false if masked cpf does not match masked patterns', () => {
+  test('should return false if masked cpf does not match masked pattern', () => {
     const sut = new CpfValidator()
 
     const invalidMaskedCpf = '12.3456.789-10'
     const isValid = sut.checkValidity(invalidMaskedCpf)
+    expect(isValid).toBe(false)
+  })
+
+  test('should return false if numbers only cpf does not match pattern', () => {
+    const sut = new CpfValidator()
+
+    const invalidCpf = '1234567891'
+    const isValid = sut.checkValidity(invalidCpf)
     expect(isValid).toBe(false)
   })
 })
