c3974ccbbc26636908f1f9678a56ed3850842b74
Tiago Gomes <tidgomes@gmail.com>
2024-01-30 11:50:49 -0300

    test: ensure RegisterController returns 400 if no cpf is provided

--- src/presentation/controller/register.spec.ts
+++ src/presentation/controller/register.spec.ts
@@ -14,4 +14,18 @@ describe('RegisterController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing Param: name'))
   })
+
+  test('should return 400 no CPF is provided', () => {
+    const sut = new RegisterController()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        birthDate: '01/01/1994'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing Param: cpf'))
+  })
 })
