07a1d542a286ca6d3c820bd7ba18473328c3d4c4
Tiago Gomes <tidgomes@gmail.com>
2024-01-30 17:21:54 -0300

    feat: ensure CpfValidator returns false if first dígito verificador is incorrect

--- src/presentation/utils/cpf-validator.ts
+++ src/presentation/utils/cpf-validator.ts
@@ -1,6 +1,26 @@
 import type { CpfValidator as CpfValidatorInterface } from '../protocols/cpf-validator'
 
 export class CpfValidator implements CpfValidatorInterface {
+  private checkDigit (cpfSlice: string[], digit: string): boolean {
+    const multiplyArray = cpfSlice.length === 9 ? [10, 9, 8, 7, 6, 5, 4, 3, 2] : [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+    const productsArray = cpfSlice.map((digit, index) => +digit * multiplyArray[index])
+
+    const sum = productsArray.reduce((acc, product) => {
+      return acc + product
+    }, 0)
+    const remainder = sum % 11
+
+    let correctDigit
+    if (remainder < 2) {
+      correctDigit = 0
+    } else if (remainder >= 2) {
+      correctDigit = 11 - remainder
+    }
+
+    if (+digit === correctDigit) return true
+    else return false
+  }
+
   checkValidity (cpf: string): boolean {
     const maskRegex = /^\d\d\d\.\d\d\d\.\d\d\d-\d\d$/
     const matchesMask = maskRegex.test(cpf)
@@ -8,5 +28,18 @@ export class CpfValidator implements CpfValidatorInterface {
     const numbersOnlyRegex = /^\d\d\d\d\d\d\d\d\d\d\d$/
     const matchesNumbersOnly = numbersOnlyRegex.test(cpf)
     if (!matchesMask && !matchesNumbersOnly) return false
+
+    let completeCpfArray
+    if (matchesMask) {
+      completeCpfArray = cpf.split('.' || '-')
+    } else if (matchesNumbersOnly) {
+      completeCpfArray = cpf.split('')
+    }
+
+    const checkFirstDigitCpfArray = completeCpfArray.slice(0, completeCpfArray.length - 2)
+    const firstDigitToVerificador = completeCpfArray[completeCpfArray.length - 2]
+
+    const firstDigitIsValid = this.checkDigit(checkFirstDigitCpfArray, firstDigitToVerificador)
+    if (!firstDigitIsValid) return false
   }
 }
