e7475fcef9bf2bb5246dc47a65e9ec6d9607c2b0
Tiago Gomes <tidgomes@gmail.com>
2024-01-30 12:00:52 -0300

    refactor: create helper function to return a bad request

--- src/presentation/controller/register.ts
+++ src/presentation/controller/register.ts
@@ -1,15 +1,13 @@
 import type { HttpRequest, HttpResponse } from '../protocols/http-protocol'
 import { MissingParamError } from '../../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 
 export class RegisterController {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredParams = ['name', 'cpf', 'birthDate']
     for (const param of requiredParams) {
       if (!httpRequest.body[param]) {
-        return {
-          statusCode: 400,
-          body: new MissingParamError(param)
-        }
+        return badRequest(new MissingParamError(param))
       }
     }
   }
