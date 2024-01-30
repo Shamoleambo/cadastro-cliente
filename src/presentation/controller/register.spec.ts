90359d7b7f72bf60a28bc77a6ea26f21640d1223
Tiago Gomes <tidgomes@gmail.com>
2024-01-30 11:54:40 -0300

    test: ensure RegisterController returns 400 if no birthDate is provided

--- src/presentation/controller/register.spec.ts
+++ src/presentation/controller/register.spec.ts
@@ -1,4 +1,5 @@
 import { RegisterController } from './register'
+import { MissingParamError } from '../../errors/missing-param-error'
 
 describe('RegisterController', () => {
   test('should return 400 if no name is provided', () => {
@@ -12,7 +13,7 @@ describe('RegisterController', () => {
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing Param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   test('should return 400 no CPF is provided', () => {
@@ -26,6 +27,20 @@ describe('RegisterController', () => {
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing Param: cpf'))
+    expect(httpResponse.body).toEqual(new MissingParamError('cpf'))
+  })
+
+  test('should return 400 if no birthDate is provided', () => {
+    const sut = new RegisterController()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        cpf: '999.999.999-00'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('birthDate'))
   })
 })
