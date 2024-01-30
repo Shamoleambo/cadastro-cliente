8feb7d6bb5e2e9e4ac9eb5df4d583600f0c2d3d4
Tiago Gomes <tidgomes@gmail.com>
2024-01-30 12:26:16 -0300

    test: ensure RegisterController returns 422 if no valid cpf is provided

--- src/presentation/controller/register.spec.ts
+++ src/presentation/controller/register.spec.ts
@@ -1,9 +1,27 @@
+import type { CpfValidator } from '../protocols/cpf-validator'
 import { RegisterController } from './register'
 import { MissingParamError } from '../../errors/missing-param-error'
 
+interface SutTypes {
+  sut: RegisterController
+  cpfValidatorStub: CpfValidator
+}
+
+const makeSut = (): SutTypes => {
+  class CpfValidatorStub implements CpfValidator {
+    checkValidity (cpf: string): boolean {
+      return true
+    }
+  }
+
+  const cpfValidatorStub = new CpfValidatorStub()
+  const sut = new RegisterController(cpfValidatorStub)
+  return { sut, cpfValidatorStub }
+}
+
 describe('RegisterController', () => {
   test('should return 400 if no name is provided', () => {
-    const sut = new RegisterController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         cpf: '999.999.999-00',
@@ -17,7 +35,7 @@ describe('RegisterController', () => {
   })
 
   test('should return 400 no CPF is provided', () => {
-    const sut = new RegisterController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'valid_name',
@@ -31,7 +49,7 @@ describe('RegisterController', () => {
   })
 
   test('should return 400 if no birthDate is provided', () => {
-    const sut = new RegisterController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'valid_name',
@@ -42,5 +60,22 @@ describe('RegisterController', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('birthDate'))
+  })
+
+  test('should return 422 if an invalid CPF is provided', () => {
+    const { sut, cpfValidatorStub } = makeSut()
+    jest.spyOn(cpfValidatorStub, 'checkValidity').mockReturnValueOnce(false)
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        cpf: '000.000.000-00',
+        birthDate: '01/01/1994'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(422)
+    expect(httpResponse.body).toEqual(new Error('Invalid CPF'))
   })
 })
