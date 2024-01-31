7ac0a53fb5e03d0a99f51a5660e3c0bb9a25b0f4
Tiago Gomes <tidgomes@gmail.com>
2024-01-31 14:37:04 -0300

    refactor: connect to MongoDB before integration tests

--- src/main/config/routes.test.ts
+++ src/main/config/routes.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
 import app from './app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('Router', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(global.__MONGO_URI__)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const clientCollection = MongoHelper.getCollection('clients')
+    await clientCollection.deleteMany({})
+  })
+
   test('should return a client on success', async () => {
     await request(app).post('/api/register')
       .send({
