d88529779ba9fb6eb0da448d0322a0655de218fc
Tiago Gomes <tidgomes@gmail.com>
2024-01-31 14:56:10 -0300

    feat: add Register factory

---
+++ src/main/factories/register.ts
@@ -0,0 +1,11 @@
+import { RegisterController } from '../../presentation/controller/register'
+import { CpfValidator } from '../../presentation/utils/cpf-validator'
+import { DbAddClient } from '../../data/usecases/db-add-client'
+import { ClientMongoRepository } from '../../infra/db/mongodb/client-repository/client'
+
+export const makeRegisterController = (): RegisterController => {
+  const clientRepository = new ClientMongoRepository()
+  const dbAddClient = new DbAddClient(clientRepository)
+  const cpfValidator = new CpfValidator()
+  return new RegisterController(cpfValidator, dbAddClient)
+}
