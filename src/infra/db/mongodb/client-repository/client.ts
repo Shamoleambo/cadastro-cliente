2ec53ae229542d09ff8adaf206082cd6d6c95ae9
Tiago Gomes <tidgomes@gmail.com>
2024-02-01 10:22:08 -0300

    feat: ensure Client Mongo Repository returns a client when provided with a cpf

--- src/infra/db/mongodb/client-repository/client.ts
+++ src/infra/db/mongodb/client-repository/client.ts
@@ -1,15 +1,24 @@
 import type { AddClientRepository } from '../../../../data/protocols/add-client-repository'
+import type { GetClientRepository } from '../../../../data/protocols/get-client-repository'
 import type { ClientModel } from '../../../../domain/models/client-model'
 import type { AddClientModel } from '../../../../domain/useCases/add-client'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class ClientMongoRepository implements AddClientRepository {
+export class ClientMongoRepository implements AddClientRepository, GetClientRepository {
   async add (clientData: AddClientModel): Promise<ClientModel> {
     const clientCollection = MongoHelper.getCollection('clients')
     const result = await clientCollection.insertOne(clientData)
     const clientId = result.insertedId
 
     const { _id, name, cpf, birthDate } = await clientCollection.findOne({ _id: clientId })
+    const client = { id: _id.toString(), name, cpf, birthDate }
+    return client
+  }
+
+  async get (cpf: string): Promise<ClientModel> {
+    const clientCollection = MongoHelper.getCollection('clients')
+
+    const { _id, name, birthDate } = await clientCollection.findOne({ cpf })
     const client = { id: _id.toString(), name, cpf, birthDate }
     return client
   }
