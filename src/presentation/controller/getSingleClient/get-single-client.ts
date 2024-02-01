2f997034add3a5d0ae4474b2fde6ef5e33b9c481
Tiago Gomes <tidgomes@gmail.com>
2024-02-01 09:20:32 -0300

    feat: ensure GetSingleClient returns 422 if an invalid cpf is provided

--- src/presentation/controller/getSingleClient/get-single-client.ts
+++ src/presentation/controller/getSingleClient/get-single-client.ts
@@ -1,11 +1,21 @@
 import { MissingParamError } from '../../../errors/missing-param-error'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, invalidCpf } from '../../helpers/http-helper'
 import type { Controller } from '../../protocols/controller-protocol'
+import type { CpfValidator } from '../../protocols/cpf-validator'
 import type { HttpRequest, HttpResponse } from '../../protocols/http-protocol'
 
 export class GetSingleClient implements Controller {
+  private readonly cpfValidator: CpfValidator
+
+  constructor (cpfValidator: CpfValidator) {
+    this.cpfValidator = cpfValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const cpf = httpRequest.body.cpf
     if (!cpf) return badRequest(new MissingParamError('cpf'))
+
+    const isValid = this.cpfValidator.checkValidity(cpf)
+    if (!isValid) return invalidCpf()
   }
 }
