66b3e5379570de72b9c5d138cac2f9b01334e5f3
Tiago Gomes <tidgomes@gmail.com>
2024-02-01 12:33:54 -0300

    test: make sure GetAllClients return 200 in success case

--- src/presentation/controller/getClients/get-clients.spec.ts
+++ src/presentation/controller/getClients/get-clients.spec.ts
@@ -30,4 +30,12 @@ describe('GetAllClients', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('should return 200 in success case', async () => {
+    const { sut } = makeSut()
+
+    const httpResponse = await sut.handle()
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual([{ id: 'valid_id', name: 'valid_name', cpf: '111.111.111-11', birthDate: '01/01/1994' }])
+  })
 })
