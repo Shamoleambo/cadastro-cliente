edaa4fb751902f702bbc5fa3aa3d34432d700a1d
Tiago Gomes <tidgomes@gmail.com>
2024-02-01 09:20:56 -0300

    test: ensure GetSingleClient returns 422 if an invalid cpf is provided

--- src/presentation/controller/getSingleClient/get-single-client.spec.ts
+++ src/presentation/controller/getSingleClient/get-single-client.spec.ts
@@ -1,9 +1,27 @@
 import { GetSingleClient } from './get-single-client'
 import { MissingParamError } from '../../../errors/missing-param-error'
+import type { CpfValidator } from '../../protocols/cpf-validator'
+
+interface SutTypes {
+  sut: GetSingleClient
+  cpfValidatorStub: CpfValidator
+}
+
+const makeSut = (): SutTypes => {
+  class CpfValidatorStub implements CpfValidator {
+    checkValidity (cpf: string): boolean {
+      return true
+    }
+  }
+
+  const cpfValidatorStub = new CpfValidatorStub()
+  const sut = new GetSingleClient(cpfValidatorStub)
+  return { sut, cpfValidatorStub }
+}
 
 describe('GetSingleClient', () => {
   test('should return 400 if no CPF is provided', async () => {
-    const sut = new GetSingleClient()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {}
     }
@@ -11,5 +29,20 @@ describe('GetSingleClient', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('cpf'))
+  })
+
+  test('should return 400 if an invalid CPF is provided', async () => {
+    const { sut, cpfValidatorStub } = makeSut()
+    jest.spyOn(cpfValidatorStub, 'checkValidity').mockImplementationOnce((cpf: string) => false)
+
+    const httpRequest = {
+      body: {
+        cpf: '11111111110'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(422)
+    expect(httpResponse.body).toEqual(new Error('Invalid CPF'))
   })
 })
