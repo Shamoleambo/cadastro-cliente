127ef26ee76f04a69e7bfee0bebbeebeb9499f42
Tiago Gomes <tidgomes@gmail.com>
2024-02-01 11:26:16 -0300

    test: ensure CpfFormatter returns a formatted cpf if it is masked

--- src/presentation/utils/cpf-formatter.spec.ts
+++ src/presentation/utils/cpf-formatter.spec.ts
@@ -8,4 +8,12 @@ describe('CpfFormatter', () => {
 
     expect(formattedCpf).toBe('11111111111')
   })
+
+  test('should return a formatted cpf if it is masked', () => {
+    const sut = new CpfFormatter()
+    const cpf = '111.111.111-11'
+    const formattedCpf = sut.format(cpf)
+
+    expect(formattedCpf).toBe('11111111111')
+  })
 })
