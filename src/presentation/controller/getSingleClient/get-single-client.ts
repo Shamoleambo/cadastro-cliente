3b117b9489dd4e4fb7f53d78592124c6dbb7b9b8
Tiago Gomes <tidgomes@gmail.com>
2024-02-01 09:39:49 -0300

    feat: ensure GetSingleClient return 500 if GetClient throws

--- src/presentation/controller/getSingleClient/get-single-client.ts
+++ src/presentation/controller/getSingleClient/get-single-client.ts
@@ -1,21 +1,30 @@
 import { MissingParamError } from '../../../errors/missing-param-error'
-import { badRequest, invalidCpf } from '../../helpers/http-helper'
+import { badRequest, invalidCpf, serverError } from '../../helpers/http-helper'
+import type { GetClient } from '../../../domain/useCases/get-client'
 import type { Controller } from '../../protocols/controller-protocol'
 import type { CpfValidator } from '../../protocols/cpf-validator'
 import type { HttpRequest, HttpResponse } from '../../protocols/http-protocol'
 
 export class GetSingleClient implements Controller {
   private readonly cpfValidator: CpfValidator
+  private readonly getClient: GetClient
 
-  constructor (cpfValidator: CpfValidator) {
+  constructor (cpfValidator: CpfValidator, getClient: GetClient) {
     this.cpfValidator = cpfValidator
+    this.getClient = getClient
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const cpf = httpRequest.body.cpf
-    if (!cpf) return badRequest(new MissingParamError('cpf'))
+    try {
+      const cpf = httpRequest.body.cpf
+      if (!cpf) return badRequest(new MissingParamError('cpf'))
 
-    const isValid = this.cpfValidator.checkValidity(cpf)
-    if (!isValid) return invalidCpf()
+      const isValid = this.cpfValidator.checkValidity(cpf)
+      if (!isValid) return invalidCpf()
+
+      await this.getClient.getClient()
+    } catch (error) {
+      return serverError()
+    }
   }
 }
