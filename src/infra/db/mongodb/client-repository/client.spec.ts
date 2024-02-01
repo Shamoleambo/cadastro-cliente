075f7afd111810942778feeacfe02767125569fe
Tiago Gomes <tidgomes@gmail.com>
2024-02-01 14:51:19 -0300

    test: implement pagination

--- src/infra/db/mongodb/client-repository/client.spec.ts
+++ src/infra/db/mongodb/client-repository/client.spec.ts
@@ -53,9 +53,28 @@ describe('Client Mongo Repository', () => {
       cpf: '11111111111',
       birthDate: '01/01/1994'
     })
-    const clients = await sut.getAll()
+    const clients = await sut.getAll(0)
 
     expect(clients).toBeTruthy()
     expect(clients.length).toBe(1)
+  })
+
+  test('should get 3 clients maximum', async () => {
+    const sut = new ClientMongoRepository()
+
+    const client1 = { name: 'any_name', cpf: '111.111.111-11', birthDate: '01/01/1994' }
+    const client2 = { name: 'any_name', cpf: '864.115.210-50', birthDate: '01/01/1994' }
+    const client3 = { name: 'any_name', cpf: '099.568.720-08', birthDate: '01/01/1994' }
+    const client4 = { name: 'any_name', cpf: '085.799.200-78', birthDate: '01/01/1994' }
+
+    await sut.add(client1)
+    await sut.add(client2)
+    await sut.add(client3)
+    await sut.add(client4)
+
+    const clients = await sut.getAll(1)
+
+    expect(clients).toBeTruthy()
+    expect(clients.length).toBeLessThanOrEqual(3)
   })
 })
