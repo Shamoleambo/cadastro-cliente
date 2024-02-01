43ac0ef0596ffda1ca2b49568f33d0df5f95f9a0
Tiago Gomes <tidgomes@gmail.com>
2024-02-01 09:40:13 -0300

    test: ensure GetSingleClient return 500 if GetClient throws

--- src/presentation/controller/getSingleClient/get-single-client.spec.ts
+++ src/presentation/controller/getSingleClient/get-single-client.spec.ts
@@ -1,22 +1,46 @@
 import { GetSingleClient } from './get-single-client'
 import { MissingParamError } from '../../../errors/missing-param-error'
+import { ServerError } from '../../../errors/server-error'
 import type { CpfValidator } from '../../protocols/cpf-validator'
+import type { ClientModel } from '../../../domain/models/client-model'
+import type { GetClient } from '../../../domain/useCases/get-client'
 
-interface SutTypes {
-  sut: GetSingleClient
-  cpfValidatorStub: CpfValidator
+const makeGetClientStub = (): GetClient => {
+  class GetClientStub implements GetClient {
+    async getClient (): Promise<ClientModel> {
+      const fakeClient = {
+        id: 'valid_id',
+        name: 'any_name',
+        cpf: '111.111.111-11',
+        birthDate: '01/01/1994'
+      }
+      return fakeClient
+    }
+  }
+
+  return new GetClientStub()
 }
 
-const makeSut = (): SutTypes => {
+const makeCpfValidatorStub = (): CpfValidator => {
   class CpfValidatorStub implements CpfValidator {
     checkValidity (cpf: string): boolean {
       return true
     }
   }
 
-  const cpfValidatorStub = new CpfValidatorStub()
-  const sut = new GetSingleClient(cpfValidatorStub)
-  return { sut, cpfValidatorStub }
+  return new CpfValidatorStub()
+}
+interface SutTypes {
+  sut: GetSingleClient
+  cpfValidatorStub: CpfValidator
+  getClientStub: GetClient
+}
+
+const makeSut = (): SutTypes => {
+  const getClientStub = makeGetClientStub()
+  const cpfValidatorStub = makeCpfValidatorStub()
+  const sut = new GetSingleClient(cpfValidatorStub, getClientStub)
+  return { sut, cpfValidatorStub, getClientStub }
 }
 
 describe('GetSingleClient', () => {
@@ -33,7 +57,7 @@ describe('GetSingleClient', () => {
 
   test('should return 400 if an invalid CPF is provided', async () => {
     const { sut, cpfValidatorStub } = makeSut()
-    jest.spyOn(cpfValidatorStub, 'checkValidity').mockImplementationOnce((cpf: string) => false)
+    jest.spyOn(cpfValidatorStub, 'checkValidity').mockReturnValueOnce(false)
 
     const httpRequest = {
       body: {
@@ -44,5 +68,19 @@ describe('GetSingleClient', () => {
 
     expect(httpResponse.statusCode).toBe(422)
     expect(httpResponse.body).toEqual(new Error('Invalid CPF'))
+  })
+
+  test('should return 500 if GetClient throws', async () => {
+    const { sut, getClientStub } = makeSut()
+    jest.spyOn(getClientStub, 'getClient').mockRejectedValueOnce(new ServerError())
+
+    const httpRequest = {
+      body: {
+        cpf: '111.111.111-11'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
