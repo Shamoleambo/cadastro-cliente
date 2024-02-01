22eb74dcc16f47dd2ce2685e7264a8b37e14a090
Tiago Gomes <tidgomes@gmail.com>
2024-02-01 12:30:30 -0300

    test: make sure GetAllClients return 500 if GetClients throws

---
+++ src/presentation/controller/getClients/get-clients.spec.ts
@@ -0,0 +1,33 @@
+import type { ClientModel } from '../../../domain/models/client-model'
+import type { GetClients } from '../../../domain/useCases/get-clients'
+import { ServerError } from '../../../errors/server-error'
+import { GetAllClients } from './get-clients'
+
+interface SutTypes {
+  sut: GetAllClients
+  getClients: GetClients
+}
+
+const makeSut = (): SutTypes => {
+  class GetClientsStub implements GetClients {
+    async getAllClients (): Promise<ClientModel[]> {
+      return [{ id: 'valid_id', name: 'valid_name', cpf: '111.111.111-11', birthDate: '01/01/1994' }]
+    }
+  }
+  const getClients = new GetClientsStub()
+  const sut = new GetAllClients(getClients)
+  return { sut, getClients }
+}
+
+describe('GetAllClients', () => {
+  test('should return 500 if GetClients throws', async () => {
+    const { sut, getClients } = makeSut()
+    jest.spyOn(getClients, 'getAllClients').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => { reject(new ServerError()) })
+    })
+
+    const httpResponse = await sut.handle()
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+})
