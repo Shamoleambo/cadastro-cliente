715d544e3c169fb39def2819f2c84def74af7717
Tiago Gomes <tidgomes@gmail.com>
2024-03-05 10:31:13 -0300

    refactor: change environment variable setting

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: global.__MONGO_URI__ || 'mongodb://127.0.0.1:27017/clientes',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/clientes',
   port: process.env.PORT || 5050
 }
